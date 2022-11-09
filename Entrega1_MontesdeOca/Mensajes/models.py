e2aaa7c1e80cba14eb8fb96608736368696e6a70
faristi <fedearistimuno@gmail.com>
2022-11-08 23:37:51 -0300

    Primer Entrega

--- Entrega1_MontesdeOca/Mensajes/models.py
+++ Entrega1_MontesdeOca/Mensajes/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Mensaje(models.Model): 
 
-    nombre=models.CharField(max_length=40) 
+    remitente=models.CharField(max_length=40)
+    destinatario=models.ForeignKey(User, on_delete=models.CASCADE)
     camada=models.IntegerField() 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# WebDjango
+Entrega Final
+(AUN SIN FINALIZAR)
+
+Participantes:
+Francisco Apa
+Fernando Montes de Oca
+Federico Aristimuño
